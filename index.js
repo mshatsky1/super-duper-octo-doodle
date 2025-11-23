faec3ea0953567062169282df85f0a43a97afbdc
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-11-23 09:24:02 +0200

    Integrate logger into main entry point

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const config = require('./src/config');
 const { formatMessage } = require('./utils/helpers');
+const logger = require('./src/logger');
 
+logger.info(`Starting ${config.appName} v${config.version}`);
 console.log(formatMessage(`Welcome to ${config.appName} v${config.version}`));
 
