d8bb9690f742d3dde5e8114ef6580fa0eff0c0f9
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-02 17:40:42 +0200

    Add sanitizeInput helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,6 +9,13 @@ function validateInput(input) {
   return input && typeof input === 'string' && input.trim().length > 0;
 }
 
+function sanitizeInput(input) {
+  if (!input || typeof input !== 'string') {
+    return '';
+  }
+  return input.trim();
+}
+
 module.exports = {
   formatMessage,
   validateInput
