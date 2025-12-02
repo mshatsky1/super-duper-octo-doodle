4e59cf6e3d1f4df81f9c56eecac259eff2cb9253
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-02 17:41:09 +0200

    Add DEFAULT_TIMEOUT constant

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,14 @@
 const DEFAULT_LOG_LEVEL = 'info';
 const MAX_STRING_LENGTH = 1000;
 const PRECISION = 10;
+const DEFAULT_TIMEOUT = 5000;
 
 module.exports = {
   DEFAULT_LOG_LEVEL,
   MAX_STRING_LENGTH,
-  PRECISION
+  PRECISION,
+  DEFAULT_TIMEOUT
 };
+
+
 
