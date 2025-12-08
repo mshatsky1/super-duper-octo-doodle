2513f1eb686a846254cba6a56538f6981b685f34
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-08 19:35:15 +0200

    feat: initialize logger based on debug config

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const config = require('./src/config');
 const { formatMessage } = require('./utils/helpers');
 const logger = require('./src/logger');
 
+// Initialize logger level based on config
+if (config.debug) {
+  logger.setLogLevel('debug');
+}
+
 logger.info(`Starting ${config.appName} v${config.version}`);
 console.log(formatMessage(`Welcome to ${config.appName} v${config.version}`));
 
