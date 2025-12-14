315a2fc7a3297a533e634d2c26a4d34e8b97aa2f
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-14 07:41:51 +0200

    feat: add port configuration option

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   appName: 'Super Duper Octo Doodle',
   version: '1.0.0',
   environment: process.env.NODE_ENV || 'development',
-  debug: process.env.DEBUG === 'true' || false
+  debug: process.env.DEBUG === 'true' || false,
+  port: process.env.PORT || 3000
 };
