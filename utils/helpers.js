cee70168187ed30e9679e89f28a13d904bdbb850
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-15 12:17:32 +0200

    feat: add randomId helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -51,6 +51,15 @@ function safeJsonParse(value, fallback = null) {
   }
 }
 
+function randomId(length = 8) {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
 module.exports = {
   formatMessage,
   validateInput,
@@ -58,6 +67,7 @@ module.exports = {
   capitalizeFirst,
   isEmpty,
   delay,
-  safeJsonParse
+  safeJsonParse,
+  randomId
 };
 
